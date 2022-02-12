0254194f1091f83f60067e18ef4f178799fc3520
Shantanu Kulkarni <heyshantu13@gmail.com>
2022-02-12 11:58:15 +0530

    Update

---
+++ src/components/Card/ServiceCard.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import {CardContent,CardMedia,Typography,Button} from "@mui/material/";
+
+const ServiceCard = (props) => {
+    const { id, title, image } = props;
+    return(
+        <>
+        <Button key={id} sx={{ mr: 2, display: "block", mt: 8 }}>
+          <CardMedia style={{ height: "94px", width: "94px" }} image={image} />
+          <CardContent>
+            <Typography
+              variant="body2"
+              component="p"
+              key={id}
+              style={{
+                fontWeight: "600",
+                fontSize: "18px",
+                color: "#000000",
+                textAlign: "center",
+                marginLeft: "-10px",
+              }}
+            >
+              {title}
+            </Typography>
+          </CardContent>
+        </Button>
+      </>
+    )
+
+}
+
+export default ServiceCard;
