c708830095ac86ffef440a1ccfc3273893817187
Shantanu Kulkarni <heyshantu13@gmail.com>
2022-02-13 19:04:23 +0530

    Create sigup reducer

---
+++ src/constants/consts.js
@@ -0,0 +1,1 @@
+export const API = "https://18.118.208.14/api/v1/";
--- src/store/app.js
+++ src/store/app.js
@@ -1,5 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
+import signupReducer from '../features/signup/signupSlice';
+
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    signup : signupReducer
+  },
 })
