72176087f47a094a31e35f32d6f10060d09f4734
Shantanu Kulkarni <heyshantu13@gmail.com>
2022-03-02 15:07:25 +0530

    Update and fix redux state for incorrect password

--- src/features/login/login.js
+++ src/features/login/login.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Box,
   Grid,
@@ -6,38 +6,41 @@ import {
   Button,
   Typography,
   CircularProgress,
-  Skeleton
 } from "@mui/material/";
-import { useDispatch, useSelector, } from "react-redux";
+import { useDispatch } from "react-redux";
 import { login } from "./loginSlice";
 import styles from "../../assets/main.module.css";
 import customStyle from "./style";
 import registerBackground from "../../assets/auth_banner.png";
 import muiStyle from "../../assets/mui_style";
-import {useNavigate } from "react-router-dom";
-import { toast } from 'react-toastify';
-
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Login = () => {
-  
   const classes = customStyle();
   const dispatch = useDispatch();
   const muistyle = muiStyle();
   const navigate = useNavigate();
-  const notify = () => toast.success("Successfully Loggedin ",{
-    position: "bottom-center",
-    autoClose: 2000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: false,
-    draggable: false,
-    progress: undefined,
+  const notify = (msg) =>
+    toast.success(msg, {
+      position: "bottom-center",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: false,
+      progress: undefined,
     });
- 
-  const [assets,setAssets] = useState({
-    background : false,
-  });
-
+  const alert = (msg) =>
+    toast.error(msg, {
+      position: "bottom-center",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: false,
+      progress: undefined,
+    });
 
   const [inputs, setInputs] = useState({
     email: "pesto@email.com",
@@ -48,59 +51,52 @@ const Login = () => {
     email: true,
     password: true,
     disableButton: false,
-    auth : true,
+    auth: true,
     passwordError: "",
     emailError: "",
     authError: "",
-    success:false,
+    success: false,
   });
 
-  const  [bntloading, setBtnloading ] = useState(false);
-
-  useEffect(() => {
-    setAssets((prevState) => ({
-      ...prevState,
-      background:true,
-    }))
-  }, []);
+  const [bntloading, setBtnloading] = useState(false);
 
   const handleSubmit = (e) => {
-    const {email, password} =  inputs;
-    if(valid.email === true && valid.password === true){
+    const { email, password } = inputs;
+    if (valid.email === true && valid.password === true) {
       e.preventDefault();
       setBtnloading(true);
       setValid((prevState) => ({
         ...prevState,
-        disableButton:true,
-        auth:true,
-        authError:"",
-      }))
-       dispatch(login({ email, password }))
-       .then((response) => {
-        notify();
-         if(response.payload.user.status === true){
+        disableButton: true,
+        auth: true,
+        authError: "",
+      }));
+      dispatch(login({ email, password }))
+        .then((response) => {
+          if (response.payload.user.status === true) {
+            notify(`Welcome Back, ${response.payload.user.data.fullname}`);
+            setTimeout(() => {
+              //history.push("/");
+              navigate("/");
+            }, 100);
+            setBtnloading(false);
+          } else {
+            alert("Opps! You Entered Incorrect Credentials");
+            setBtnloading(false);
+            setValid({
+              disableButton: false,
+            });
+          }
+        })
+        .catch((error) => {
           setBtnloading(false);
-         }
-         
-         setTimeout(() => {
-           //history.push("/");
-           navigate("/");
-        }, 100);
-       
-       }).catch((error) => {
-        setBtnloading(false);
-        setValid((prevState) => ({
-          ...prevState,
-          disableButton:false,
-          authError:"Email or Password is incorrect",
-          auth:false,
-        }));
-
-       });
-    }else{
+          setValid({
+            disableButton: false,
+          });
+        });
+    } else {
       e.preventDefault();
     }
-   
   };
 
   const handleChange = (e) => {
@@ -113,11 +109,11 @@ const Login = () => {
     if (e.target.name === "password") {
       validatePass(e.target.value);
     }
-
   };
 
   const validateEmail = (email) => {
-    const pattern = /[a-zA-Z0-9]+[\.]?([a-zA-Z0-9]+)?[\@][a-z]{3,9}[\.][a-z]{2,5}/g;
+    const pattern =
+      /[a-zA-Z0-9]+[\.]?([a-zA-Z0-9]+)?[\@][a-z]{3,9}[\.][a-z]{2,5}/g;
     const result = pattern.test(email);
     if (result === false) {
       setValid((prevState) => ({
@@ -155,106 +151,85 @@ const Login = () => {
           lg={6}
           item
         >
-          { assets.background ?
-           <Box
+          <Box
             style={{
-              height: "42rem",
+              height: "41.5rem",
               maxWidth: "44rem",
               backgroundSize: "cover",
               backgroundBlendMode: "overlay",
               backgroundColor: "rgba(45, 45, 45, 0.55)",
               backgroundImage: `url(${registerBackground})`,
-            
             }}
           />
-          :
-          <Skeleton variant="rectangular" style={{
-            height: "42rem",
-            maxWidth: "44rem",
-           }} />
-        }
         </Grid>
-        <Grid sm={12} xs={12} md={6} lg={6} item >
-        <Box component="span" className={classes.authform} sx={{
-              "& > :not(style)": { m: 1, maxWidth: "65ch"},
-            }}>
-        <Grid sm={12} xs={12} md={12} lg={12} item m={4}>
-        <Typography  variant="h4" className={classes.auth_head} m={6} mt={8}>
-           Login to your account
-        </Typography>
-        </Grid>
-          <form onSubmit={handleSubmit} className={styles.form_align}>
-          <Grid sm={12} xs={12} md={12} lg={12} item m={3}>
-            <TextField
-              required
-              onChange={handleChange}
-              id="email"
-              label="Email"
-              variant="outlined"
-              className={`field-spacing`}
-              fullWidth
-              value={inputs.email}
-              name="email"
-              error={!valid.email}
-              helperText={valid.emailError }
-            />
-            </Grid>
-            <Grid sm={12} xs={12} md={12} lg={12} item m={3}>
-            <TextField
-              required
-              id="password"
-              label="Password"
-              variant="outlined"
-              type="password"
-              className={`field-spacing `}
-              fullWidth
-              onChange={handleChange}
-              value={inputs.password}
-              name="password"
-              error={!valid.password}
-              helperText={valid.passwordError}
-            />
-            </Grid>
-            <Grid sm={12} xs={12} md={12} lg={12} item m={3}>
-                 {!valid.auth
-                  ?  <Typography
-                  variant="string"
-                  align="center"
-                  className={` ${muistyle.wd_text} ${muistyle.wd_danger_text}`}
-                >
-                  {valid.authError}
-                </Typography>
-                : ""
-                 }
-            </Grid>
-            <Grid sm={12} xs={12} md={12} lg={12} item m={3}>
-           <Typography
-              variant="string"
-              align="center"
-              className={`field-spacing ${classes.forget_pass_text}`}
-            >
-              Forget Password?
+        <Grid sm={12} xs={12} md={6} lg={6} item>
+          <Grid sm={12} xs={12} md={12} lg={12} item m={4}>
+            <Typography variant="h4" className={classes.auth_head} m={6} mt={8}>
+              Welcome To Workers Deck
             </Typography>
-
+          </Grid>
+          <form onSubmit={handleSubmit} className={styles.form_align}>
+            <Grid sm={12} xs={12} md={8} lg={8} item>
+              <TextField
+                required
+                onChange={handleChange}
+                id="email"
+                label="Email"
+                variant="outlined"
+                className={`field-spacing`}
+                fullWidth
+                value={inputs.email}
+                name="email"
+                error={!valid.email}
+                helperText={valid.emailError}
+              />
             </Grid>
-            <Grid sm={12} xs={12} md={12} lg={12} item m={3} >
-            <Button
-              variant="contained"
-              className="auth-btn"
-              type="submit"
-              disabled={valid.disableButton}
-              className={`${classes.login_btn} ${muistyle.wd_primary_btn} ${classes.centerBox}`}
-            >
-                {bntloading 
-                    ? <CircularProgress  size={30} thickness={6}  sx={{
-                      color: '#ffffff',
-                    }}/> 
-                    : "Login"
-                    }
-            </Button>
+            <Grid sm={12} xs={12} md={8} lg={8} item>
+              <TextField
+                required
+                id="password"
+                label="Password"
+                variant="outlined"
+                type="password"
+                className={`field-spacing `}
+                fullWidth
+                onChange={handleChange}
+                value={inputs.password}
+                name="password"
+                error={!valid.password}
+                helperText={valid.passwordError}
+              />
+            </Grid>
+            <Grid sm={12} xs={12} md={8} lg={8} item>
+              <Typography
+                variant="string"
+                align="center"
+                className={`field-spacing ${classes.forget_pass_text}`}
+              >
+                Forget Password?
+              </Typography>
+            </Grid>
+            <Grid sm={6} xs={6} md={6} lg={6} item>
+              <Button
+                variant="contained"
+                className="auth-btn"
+                type="submit"
+                disabled={valid.disableButton}
+              >
+                {bntloading ? (
+                  <CircularProgress
+                    size={30}
+                    thickness={6}
+                    sx={{
+                      color: "#ffffff",
+                    }}
+                  />
+                ) : (
+                  "Login To Account"
+                )}
+              </Button>
             </Grid>
           </form>
-          </Box>
         </Grid>
       </Grid>
     </>
